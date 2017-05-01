41f68208a77e6ea862c5a19f80ab80482dc4e484
marblo <marcinblochowiak@wp.pl>
2017-05-01 12:16:25 +0200

    develop

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animalUpperCase = animal.toUpperCase(animal);
+var animalCharsAfter = text.replace('Papugi', animalUpperCase);
+var partOftext = animalCharsAfter.slice(1,45);
+
+
+
+console.log(partOftext);
+
+
+
+
+
+
+
+
+
+
+/*var motto = 'Niebieskie kanarki zawsze spadają na cztery łapy.';
+var mottoCharsAfter = motto.replace('kanarki', 'legwany'); 
+console.log(mottoCharsAfter);
+
+
+
+
+
+var motto = 'Niebieskie kanarki zawsze spadają na cztery łapy.'; var mottoCharsAfter = motto.replace('kanarki', 'legwany'); 
+console.log(mottoCharsAfter);
+
+
+var seaName = 'Marynarz Stefan'; var seaNameUpperCased = seaName.toUpperCase(); console.log(seaNameUpperCased);*/
